3ce4782e5eb11df61fd966344c02d05cb7ad605f
Ian <236912195+evo091@users.noreply.github.com>
2026-03-18 22:00:48 -0400

    began structuring and began the render

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import { useState } from "react";
+import PLANTS from "./data";
+import Cart from "./cart/Cart";
+import Plants from "./plants/Plants";
+
 export default function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (plant) => {
+    const itemExists = cart.find((i) => i.id === plant.id);
+    if (itemExists) {
+      setCart (
+        cart.map((item) =>
+          item.id === plant.id
+          ? {...item, quantity: item.quantity + 1} : item),
+      );
+    } else {
+      const item = { ...plant, quantity: 1};
+      setCart([...cart, item]);
+    }
+  };
+
+  const removeFromCart = (itemToRemove) => {
+    setCart(
+      cart
+
   return <></>;
 }
